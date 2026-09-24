363cbc7f6fde2f52ae4f5f7e8e7e6fe5f29d4778
Riddhesh <riddhesh.26bai11055@vitbhopal.ac.in>
2026-09-24 09:39:22 +0530

    Add files via upload

---
+++ rockpaperscissor2.py
@@ -0,0 +1,55 @@
+import random
+
+print("!!!!!ROCK PAPER SCISSORS!!!!")
+
+choices = ['rock', 'paper', 'scissors']
+
+playerscore = 0
+compscore = 0
+
+
+while True:
+    print("\nChoose one:")
+    print("1. rock")
+    print("2. paper")
+    print("3. scissors")
+    print("4. end")
+
+
+    choice = input("Your choice: ")
+
+    if choice == "4":
+        break
+    if choice not in ["1", "2", "3"]:
+        print("Invalid choice! Try again.")
+        continue
+
+    x = choices[int(choice) - 1]                #PLAYER'S CHOICE
+    y = random.choice(choices)                  #COMPUTER'S CHOICE
+
+    print("\nYou chose:", x)
+    print("Computer chose:", y)
+
+    if y == x:
+        print("DRAW.")
+
+    elif ((x == "rock" and y == "scissors")
+        or
+        (x == "paper" and y == "rock")
+        or
+        (x == "scissors" and y == "paper")):
+        print("WON.")
+        playerscore += 1
+
+    else:
+        print("LOSE.")
+        compscore += 1
+
+    print("Your Score:", playerscore)
+    print("Computer Score:", compscore)
+
+print("GAME OVER.")
+
+print("Final score:", playerscore, "-" ,compscore)
+
+print("Thanks! Come Again.")
